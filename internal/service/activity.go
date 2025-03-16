3c1c9cadab94cfb9a28d5f91a4aa45e48e0ef4a9
robertd2000 <zdabaghov@mail.ru>
2025-03-16 19:15:31 +0300

    NewDecoder

--- internal/service/activity.go
+++ internal/service/activity.go
@@ -1,16 +1,29 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/robertd2000/github-activity/internal/model"
 )
 
-func FetchActivity(username string) string {
+func FetchActivity(username string) []model.Activity {
 	resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/events", username))
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	return resp
+	if resp.StatusCode != 200 {
+		log.Fatalln("Failed to fetch activity")
+	}
+
+	var activities []model.Activity
+
+	if err := json.NewDecoder(resp.Body).Decode(&activities); err != nil {
+		log.Fatalln(err)
+	}
+
+	return activities
 }
